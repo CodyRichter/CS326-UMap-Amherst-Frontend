c9186eb0711fb6e36c5aadbe950a338c9faffcf6
Senarii <vortekdynamic@gmail.com>
2021-03-15 16:23:04 -0400

    Add header

--- src/App.js
+++ src/App.js
@@ -9,37 +9,38 @@ import Home from "./pages/Home";
 import Classes from "./pages/Classes";
 import Stops from "./pages/Stops";
 import Login from "./pages/Login";
+import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {Map as MapIcon} from "@material-ui/icons";
+
 
 export default function App() {
   return (
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/classes">Classes</Link>
-              </li>
-              <li>
-                <Link to="/stops">Pit Stops</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </ul>
-          </nav>
 
-          {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/classes" component={Classes} />
-            <Route path="/stops" component={Stops} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </div>
+      <Router>
+
+        <AppBar position="static">
+          <Toolbar variant="dense">
+            <Link to="/" component={IconButton} edge="start" color="inherit" aria-label="menu">
+              <MapIcon />
+            </Link>
+            <Typography variant="h6">
+              UMap Amherst
+            </Typography>
+            <Link to="/" component={Button} color={'inherit'}>Home</Link>
+            <Link to="/classes" component={Button} color={'inherit'}>Classes</Link>
+            <Link to="/stops" component={Button} color={'inherit'}>Pitstops</Link>
+            <Link to="/login" component={Button} color={'inherit'}>Login</Link>
+
+          </Toolbar>
+        </AppBar>
+
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/classes" component={Classes} />
+          <Route path="/stops" component={Stops} />
+          <Route path="/login" component={Login} />
+        </Switch>
       </Router>
   );
 }
