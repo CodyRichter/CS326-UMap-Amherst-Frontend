a48be7a604604f537846c0ad63b445861a46dc72
Senarii <vortekdynamic@gmail.com>
2021-03-15 16:34:36 -0400

    Add basic styles

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Home from "./pages/Home";
 import Classes from "./pages/Classes";
 import Stops from "./pages/Stops";
 import Login from "./pages/Login";
-import {AppBar, Button, IconButton, Toolbar, Typography} from "@material-ui/core";
-import {Map as MapIcon} from "@material-ui/icons";
+import {AppBar, Button, Grid, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {Map as MapIcon, Stop} from "@material-ui/icons";
 
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
 
       <Router>
 
-        <AppBar position="static">
+        <AppBar position="sticky">
           <Toolbar variant="dense">
             <Link to="/" component={IconButton} edge="start" color="inherit" aria-label="menu">
               <MapIcon />
@@ -26,21 +26,40 @@ export default function App() {
             <Typography variant="h6">
               UMap Amherst
             </Typography>
-            <Link to="/" component={Button} color={'inherit'}>Home</Link>
-            <Link to="/classes" component={Button} color={'inherit'}>Classes</Link>
-            <Link to="/stops" component={Button} color={'inherit'}>Pitstops</Link>
-            <Link to="/login" component={Button} color={'inherit'}>Login</Link>
-
+              <Link to="/" component={Button} color={'inherit'}>Home</Link>
+              <Link to="/classes" component={Button} color={'inherit'}>Classes</Link>
+              <Link to="/stops" component={Button} color={'inherit'}>Pitstops</Link>
+              <Link to="/login" component={Button} color={'inherit'}>Login</Link>
           </Toolbar>
         </AppBar>
 
-
         <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/classes" component={Classes} />
-          <Route path="/stops" component={Stops} />
-          <Route path="/login" component={Login} />
+          <Grid container direction="row" justify="center" alignItems="center" className={'pageContainer'}>
+            <Route path="/" exact>
+              <Grid item xs={8} className={'innerPage'}>
+                <Home />
+              </Grid>
+            </Route>
+            <Route path="/classes">
+              <Grid item xs={8} className={'innerPage'}>
+                <Classes />
+              </Grid>
+            </Route>
+            <Route path="/stops" exact>
+              <Grid item xs={8} className={'innerPage'}>
+                <Stops />
+              </Grid>
+            </Route>
+            <Route path="/login">
+              <Grid item xs={8} className={'innerPage'}>
+                <Login />
+              </Grid>
+            </Route>
+
+          </Grid>
+
         </Switch>
-      </Router>
+
+</Router>
   );
 }
