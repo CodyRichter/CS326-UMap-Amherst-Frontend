5bffbdfdf7a2dca2a35a255c227ee75a7d1af78b
gfleuri <gedeonfleurissaint@gmail.com>
2021-03-17 14:32:23 -0400

    setting up one class

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -8,6 +8,22 @@ export default function Classes() {
           <span className="classes-title">Class Schedule</span>
           <button className="classes-button">Add Class</button>
         </div>
+        <div className="classes-list">
+          <div className="classes-list-item">
+            <div>
+              COMPSCI - 121{" "}
+              <span style={{ float: "right" }}>Monday, Wednesday</span>
+            </div>
+            <div>
+              Thompson Hall{" "}
+              <span style={{ float: "right" }}>11:15 AM to 12:05 PM</span>
+            </div>
+            <div>Room 104</div>
+            <button className="classes-button">Remove</button>
+          </div>
+        </div>
+        <button className="classes-button">Save Classes</button>
+        <button className="classes-button">Return to Map</button>
       </div>
     </div>
   );
