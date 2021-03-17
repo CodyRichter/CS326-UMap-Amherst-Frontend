7e81da3eb9e87457ec1f1610b0c0a4afb5394101
gfleuri <gedeonfleurissaint@gmail.com>
2021-03-17 14:16:26 -0400

    setting up classes page

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,10 +1,14 @@
 import React from "react";
 
-
 export default function Classes() {
-    return (
-      <div>
-          Classes page
+  return (
+    <div>
+      <div className="classes-dashboard">
+        <div>
+          <span className="classes-title">Class Schedule</span>
+          <button className="classes-button">Add Class</button>
+        </div>
       </div>
-    );
+    </div>
+  );
 }
