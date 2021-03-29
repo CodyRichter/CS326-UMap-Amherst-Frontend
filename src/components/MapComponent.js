0a011303c9fdd0fd5246c8cbcad602ae7bad5e59
Cody Richter <Cody@Richter.codes>
2021-03-29 18:20:45 -0400

    Add template directions overlay

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -1,14 +1,193 @@
 import GoogleMapReact from 'google-map-react';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 
 export default function MapComponent() {
     const mapStyle = require('../mapstyle.json');
+
+
+    function mapResultToCoords(api_result) {
+        let directions = [];
+        api_result.routes[0].legs[0].steps.forEach((leg, idx) => {
+            directions.push({lat: leg.start_location.lat, lng: leg.start_location.lng })
+            directions.push({lat: leg.end_location.lat, lng: leg.end_location.lng })
+        });
+        return directions;
+    }
+
+
+    function handleLoadGoogleMapsAPI(map, maps) {
+
+        let api_res = {
+            "geocoded_waypoints": [
+                {
+                    "geocoder_status": "OK",
+                    "place_id": "ChIJ3-y273PS5okRyNNBnId51eU",
+                    "types": [
+                        "premise"
+                    ]
+                },
+                {
+                    "geocoder_status": "OK",
+                    "place_id": "ChIJqbpXyHDS5okRz_S73SlN3Vg",
+                    "types": [
+                        "premise"
+                    ]
+                }
+            ],
+            "routes": [
+                {
+                    "bounds": {
+                        "northeast": {
+                            "lat": 42.3913474,
+                            "lng": -72.52621449999999
+                        },
+                        "southwest": {
+                            "lat": 42.3878964,
+                            "lng": -72.52742599999999
+                        }
+                    },
+                    "copyrights": "Map data ©2021 Google",
+                    "legs": [
+                        {
+                            "distance": {
+                                "text": "0.3 mi",
+                                "value": 428
+                            },
+                            "duration": {
+                                "text": "5 mins",
+                                "value": 300
+                            },
+                            "end_address": "Murray D. Lincoln Campus Center, 1 Campus Center Way, Amherst, MA 01003, USA",
+                            "end_location": {
+                                "lat": 42.3913474,
+                                "lng": -72.5265107
+                            },
+                            "start_address": "Herter Hall, Amherst, MA 01002, USA",
+                            "start_location": {
+                                "lat": 42.3878964,
+                                "lng": -72.52736899999999
+                            },
+                            "steps": [
+                                {
+                                    "distance": {
+                                        "text": "0.2 mi",
+                                        "value": 304
+                                    },
+                                    "duration": {
+                                        "text": "3 mins",
+                                        "value": 209
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3905485,
+                                        "lng": -72.5267006
+                                    },
+                                    "html_instructions": "Head <b>north</b>",
+                                    "polyline": {
+                                        "points": "k{uaG`otyL_@JGGQK[QeAG{@Ge@GWAc@EaAMm@SEA[KeA["
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3878964,
+                                        "lng": -72.52736899999999
+                                    },
+                                    "travel_mode": "WALKING"
+                                },
+                                {
+                                    "distance": {
+                                        "text": "135 ft",
+                                        "value": 41
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 32
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3906333,
+                                        "lng": -72.52621449999999
+                                    },
+                                    "html_instructions": "Turn <b>right</b>",
+                                    "maneuver": "turn-right",
+                                    "polyline": {
+                                        "points": "}kvaGzjtyLGq@Go@"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3905485,
+                                        "lng": -72.5267006
+                                    },
+                                    "travel_mode": "WALKING"
+                                },
+                                {
+                                    "distance": {
+                                        "text": "272 ft",
+                                        "value": 83
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 59
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3913474,
+                                        "lng": -72.5265107
+                                    },
+                                    "html_instructions": "Turn <b>left</b><div style=\"font-size:0.9em\">Destination will be on the left</div>",
+                                    "maneuver": "turn-left",
+                                    "polyline": {
+                                        "points": "mlvaGxgtyLSDaAZ_@LYJ"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3906333,
+                                        "lng": -72.52621449999999
+                                    },
+                                    "travel_mode": "WALKING"
+                                }
+                            ],
+                            "traffic_speed_entry": [],
+                            "via_waypoint": []
+                        }
+                    ],
+                    "overview_polyline": {
+                        "points": "k{uaG`otyL_@JGGm@]aCO}@IeBSs@UaBg@OaBuA`@y@X"
+                    },
+                    "summary": "",
+                    "warnings": [
+                        "Walking directions are in beta. Use caution – This route may be missing sidewalks or pedestrian paths."
+                    ],
+                    "waypoint_order": []
+                }
+            ],
+            "status": "OK"
+        };
+
+        let directions = mapResultToCoords(api_res);
+
+        const directionPath = new maps.Polyline({
+            path: directions,
+            geodesic: true,
+            strokeColor: "#389C29",
+            strokeOpacity: 1.0,
+            strokeWeight: 3,
+        });
+        directionPath.setMap(map);
+        // map.data.add({
+        //     // geometry: new maps.Data.LineString([
+        //     //     {"lat": 42.3878658, "lng": -72.5270293},
+        //     //     {"lat": 42.3854799, "lng": -72.5258504},
+        //     //     {"lat": 42.3925702, "lng": -72.5256119},
+        //     //     {"lat": 42.391686, "lng": -72.52609099999999}
+        //     // ])
+        //     geometry: new maps.Data.Polyline([
+        //         "qxvaG`dtyLPbBJp@FAFADCDEDCFEFCFADABA@?JCHAHALALAB?BABABC"
+        //         ])
+        // })
+    }
+
+
     return (
         // Important! Always set the container height explicitly
             <GoogleMapReact
                 defaultCenter={{lat: 42.389564, lng: -72.526512}}
                 defaultZoom={16}
+                onGoogleApiLoaded={({ map, maps }) => handleLoadGoogleMapsAPI(map, maps)}
                 yesIWantToUseGoogleMapApiInternals
                 bootstrapURLKeys={{key: "AIzaSyAz2oL1-IeVDxCY7lWV2ivTZ3LIpEkrWEE"}}
                 options={{
@@ -16,11 +195,10 @@ export default function MapComponent() {
                     draggable: true, // make map draggable
                     keyboardShortcuts: false, // disable keyboard shortcuts
                     scaleControl: true, // allow scale control
-                    scrollwheel: true, // allow scroll wheel
                     styles: mapStyle, // change default map styles
                     draggableCursor: 'grab',
                     draggingCursor: 'grabbing',
-                }}
+            }}
             >
             </GoogleMapReact>
     );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -39,6 +39,200 @@ export default function Home() {
         }
     ]);
 
+    function showDirectionsOnMap() {
+
+        // let startPos = [42.387781, -72.527347];  // Herter hall
+        // let endPos = [42.391327, -72.526965];  // Campus center
+
+        // TODO: Make call to server and get directions
+
+        // This is what the API will normally return
+        let api_call_result = {
+            "geocoded_waypoints": [
+                {
+                    "geocoder_status": "OK",
+                    "place_id": "ChIJ3-y273PS5okRyNNBnId51eU",
+                    "types": [
+                        "premise"
+                    ]
+                },
+                {
+                    "geocoder_status": "OK",
+                    "place_id": "ChIJqbpXyHDS5okRz_S73SlN3Vg",
+                    "types": [
+                        "premise"
+                    ]
+                }
+            ],
+            "routes": [
+                {
+                    "bounds": {
+                        "northeast": {
+                            "lat": 42.3925702,
+                            "lng": -72.52233369999999
+                        },
+                        "southwest": {
+                            "lat": 42.3852591,
+                            "lng": -72.5270293
+                        }
+                    },
+                    "copyrights": "Map data ©2021",
+                    "legs": [
+                        {
+                            "distance": {
+                                "text": "1.0 mi",
+                                "value": 1582
+                            },
+                            "duration": {
+                                "text": "5 mins",
+                                "value": 273
+                            },
+                            "end_address": "Murray D. Lincoln Campus Center, 1 Campus Center Way, Amherst, MA 01003, USA",
+                            "end_location": {
+                                "lat": 42.391686,
+                                "lng": -72.52609099999999
+                            },
+                            "start_address": "Herter Hall, Amherst, MA 01002, USA",
+                            "start_location": {
+                                "lat": 42.3878658,
+                                "lng": -72.5270293
+                            },
+                            "steps": [
+                                {
+                                    "distance": {
+                                        "text": "397 ft",
+                                        "value": 121
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 23
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3870385,
+                                        "lng": -72.52641
+                                    },
+                                    "html_instructions": "Head <b>south</b> toward <b>Presidents Dr</b>",
+                                    "polyline": {
+                                        "points": "e{uaG|ltyLd@Q~Ai@VIBADAB?CWAY"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3878658,
+                                        "lng": -72.5270293
+                                    },
+                                    "travel_mode": "DRIVING"
+                                },
+                                {
+                                    "distance": {
+                                        "text": "0.1 mi",
+                                        "value": 179
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 76
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3854799,
+                                        "lng": -72.5258504
+                                    },
+                                    "html_instructions": "Turn <b>right</b> onto <b>Presidents Dr</b>",
+                                    "maneuver": "turn-right",
+                                    "polyline": {
+                                        "points": "_vuaG`ityLD?lA]XGbAUl@QnA]FC"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3870385,
+                                        "lng": -72.52641
+                                    },
+                                    "travel_mode": "DRIVING"
+                                },
+                                {
+                                    "distance": {
+                                        "text": "0.2 mi",
+                                        "value": 292
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 40
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3858297,
+                                        "lng": -72.52262829999999
+                                    },
+                                    "html_instructions": "Turn <b>left</b> onto <b>Massachusetts Ave</b>",
+                                    "maneuver": "turn-left",
+                                    "polyline": {
+                                        "points": "gluaGpetyLj@Sa@wCCOS}AIm@OoACQEYGa@G]CQ?CASAS?Q?Q@G?G@M"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3854799,
+                                        "lng": -72.5258504
+                                    },
+                                    "travel_mode": "DRIVING"
+                                },
+                                {
+                                    "distance": {
+                                        "text": "0.5 mi",
+                                        "value": 841
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 79
+                                    },
+                                    "end_location": {
+                                        "lat": 42.3925702,
+                                        "lng": -72.5256119
+                                    },
+                                    "html_instructions": "Turn <b>left</b> onto <b>N Pleasant St</b>",
+                                    "maneuver": "turn-left",
+                                    "polyline": {
+                                        "points": "mnuaGlqsyL@IBK@E[IQCw@OQAI?C?G?KBMFA?KFQPa@d@a@f@IJKPSZ}@tAA@KPKPCB]^URIHm@`@GDSJ[NoAb@[LmA^A?EBQDE@u@L_D\\eALK@C?cCZMBqAVk@L"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3858297,
+                                        "lng": -72.52262829999999
+                                    },
+                                    "travel_mode": "DRIVING"
+                                },
+                                {
+                                    "distance": {
+                                        "text": "489 ft",
+                                        "value": 149
+                                    },
+                                    "duration": {
+                                        "text": "1 min",
+                                        "value": 55
+                                    },
+                                    "end_location": {
+                                        "lat": 42.391686,
+                                        "lng": -72.52609099999999
+                                    },
+                                    "html_instructions": "Turn <b>left</b><div style=\"font-size:0.9em\">Destination will be on the right</div>",
+                                    "maneuver": "turn-left",
+                                    "polyline": {
+                                        "points": "qxvaG`dtyLPbBJp@FAFADCDEDCFEFCFADABA@?JCHAHALALAB?BABABC"
+                                    },
+                                    "start_location": {
+                                        "lat": 42.3925702,
+                                        "lng": -72.5256119
+                                    },
+                                    "travel_mode": "DRIVING"
+                                }
+                            ],
+                            "traffic_speed_entry": [],
+                            "via_waypoint": []
+                        }
+                    ],
+                    "overview_polyline": {
+                        "points": "e{uaG|ltyL`DgAHAEq@pD{@|Bo@r@WcAsHa@}CMgA?_AFk@@E[IiASg@AYJMF_BjB_BdCWb@a@b@_@\\u@f@o@ZkBp@gBh@{@NeFj@aD`@}Bd@\\tCZM\\OVGz@KBC"
+                    },
+                    "summary": "N Pleasant St",
+                    "warnings": [],
+                    "waypoint_order": []
+                }
+            ],
+            "status": "OK"
+        }
+    }
 
     return (
         <Grid container spacing={3}>
@@ -77,8 +271,10 @@ export default function Home() {
                                         <Typography variant={'h6'}>Upcoming Classes</Typography>
                                     </Grid>
                                     <Grid item>
-                                        <Link to="/classes" component={IconButton} size="small" variant="outlined">
-                                            <Edit />
+                                        <Link to="/classes">
+                                            <IconButton size="small" variant="outlined">
+                                                <Edit />
+                                            </IconButton>
                                         </Link>
                                     </Grid>
                                 </Grid>
@@ -99,8 +295,10 @@ export default function Home() {
                                         <Typography variant={'h6'}>Scheduled Pit Stops</Typography>
                                     </Grid>
                                     <Grid item>
-                                        <Link to="/stops" component={IconButton} size="small" variant="outlined">
-                                            <Edit />
+                                        <Link to="/stops">
+                                            <IconButton size="small" variant="outlined">
+                                                <Edit />
+                                            </IconButton>
                                         </Link>
                                     </Grid>
                                 </Grid>
