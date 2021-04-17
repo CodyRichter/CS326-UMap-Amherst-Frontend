60a89cfa33d7bcaa86adac083c2ca1daf766f057
gfleuri <gedeonfleurissaint@gmail.com>
2021-04-17 17:03:15 -0400

    loading buildings and classes into  selector

--- src/App.js
+++ src/App.js
@@ -1,65 +1,82 @@
 import React from "react";
-import {
-  HashRouter,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Classes from "./pages/Classes";
 import Stops from "./pages/Stops";
 import Login from "./pages/Login";
-import {AppBar, Button, Grid, IconButton, Toolbar, Typography} from "@material-ui/core";
-import {Map as MapIcon, } from "@material-ui/icons";
+import {
+  AppBar,
+  Button,
+  Grid,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { Map as MapIcon } from "@material-ui/icons";
 
-
-export default function App() {
+export default function App(props) {
   return (
+    <HashRouter basename={"/"}>
+      <AppBar position="sticky">
+        <Toolbar variant="dense">
+          <Link
+            to="/"
+            component={IconButton}
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+          >
+            <MapIcon />
+          </Link>
+          <Typography variant="h6">UMap Amherst</Typography>
+          <Link to="/" component={Button} color={"inherit"}>
+            Home
+          </Link>
+          <Link to="/classes" component={Button} color={"inherit"}>
+            Classes
+          </Link>
+          <Link to="/stops" component={Button} color={"inherit"}>
+            Pitstops
+          </Link>
+          <Link to="/login" component={Button} color={"inherit"}>
+            Login
+          </Link>
+        </Toolbar>
+      </AppBar>
 
-      <HashRouter basename={'/'}>
-
-        <AppBar position="sticky">
-          <Toolbar variant="dense">
-            <Link to="/" component={IconButton} edge="start" color="inherit" aria-label="menu">
-              <MapIcon />
-            </Link>
-            <Typography variant="h6">
-              UMap Amherst
-            </Typography>
-              <Link to="/" component={Button} color={'inherit'}>Home</Link>
-              <Link to="/classes" component={Button} color={'inherit'}>Classes</Link>
-              <Link to="/stops" component={Button} color={'inherit'}>Pitstops</Link>
-              <Link to="/login" component={Button} color={'inherit'}>Login</Link>
-          </Toolbar>
-        </AppBar>
-
-        <Switch>
-          <Grid container direction="row" justifyContent="center" alignItems="center" className={'pageContainer'}>
-            <Route path="/" exact>
-              <Grid item xs={10} className={'innerPage'}>
-                <Home />
-              </Grid>
-            </Route>
-            <Route path="/classes">
-              <Grid item xs={8} className={'innerPage'}>
-                <Classes />
-              </Grid>
-            </Route>
-            <Route path="/stops" exact>
-              <Grid item xs={8} className={'innerPage'}>
-                <Stops />
-              </Grid>
-            </Route>
-            <Route path="/login">
-              <Grid item xs={4} className={'innerPage'}>
-                <Login />
-              </Grid>
-            </Route>
-
-          </Grid>
-
-        </Switch>
-
-      </HashRouter>
+      <Switch>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          className={"pageContainer"}
+        >
+          <Route path="/" exact>
+            <Grid item xs={10} className={"innerPage"}>
+              <Home />
+            </Grid>
+          </Route>
+          <Route path="/classes">
+            <Grid item xs={8} className={"innerPage"}>
+              <Classes
+                availableClasses={props.availableClasses}
+                availableBuildings={props.availableBuildings}
+              />
+            </Grid>
+          </Route>
+          <Route path="/stops" exact>
+            <Grid item xs={8} className={"innerPage"}>
+              <Stops />
+            </Grid>
+          </Route>
+          <Route path="/login">
+            <Grid item xs={4} className={"innerPage"}>
+              <Login />
+            </Grid>
+          </Route>
+        </Grid>
+      </Switch>
+    </HashRouter>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,32 @@ import "./main.css";
 import "./classes.css";
 import "./login.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+async function fetchData() {
+  // Loads available classes to select from SQL
+  let availableClasses = await fetch("http://localhost:5000/classes", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => res.json());
+  // Loads available buildings to select from SQL
+  let availableBuildings = await fetch("http://localhost:5000/buildings", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  }).then((res) => res.json());
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App
+        availableClasses={availableClasses}
+        availableBuildings={availableBuildings}
+      />
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
+fetchData();
+ReactDOM.render(<span>Loading...</span>, document.getElementById("root"));
